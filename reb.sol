e9b41c6769bf7dcf0dad5552e97bdd6264b09b6b
Drew Roberts <github@drewroberts.com>
2024-04-19 03:19:26 -0400

    Add web app team wallet address

--- reb.sol
+++ reb.sol
@@ -118,7 +118,7 @@ contract JohnnyReb is Ownable {
     address payable public DrewRoberts =
         payable(0xC6aa2f0FF6b8563EA418ec2558890D6027413699);
     address payable public WebAppTeam =
-        payable(0xBc46A0F68795c90ca2A77168169b806ca440Eb26);
+        payable(0xeb6E8a1Fe7Ad4cDFa49a95ad480422c72D1Ab79C);
 
     constructor() {}
 
